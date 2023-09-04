b0ae69c66cbfc1568e0935248da6b280260e2cda
andres.mera <feli041089@gmail.com>
2023-09-04 08:58:18 -0500

    COlocando en el State los valores ingresados

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,4 +1,11 @@
 <script setup>
+import { reactive } from 'vue';
+
+const busqueda = reactive({
+    ciudad: '',
+    pais: ''
+});
+
 const paises = [
     { codigo: 'US', nombre: 'Estados Unidos' },
     { codigo: 'MX', nombre: 'México' },
@@ -14,12 +21,12 @@ const paises = [
     <form class="formulario">
         <div class="campo">
             <label for="ciudad">Ciudad</label>
-            <input type="text" id="ciudad" placeholder="Ciudad" />
+            <input type="text" id="ciudad" placeholder="Ciudad" v-model="busqueda.ciudad" />
         </div>
 
         <div class="campo">
             <label for="pais">País</label>
-            <select id="pais">
+            <select id="pais" v-model="busqueda.pais">
                 <option value="">-- Seleccione un País --</option>
                 <option v-for="pais in paises" :value="pais.codigo">{{ pais.nombre }}</option>
             </select>
