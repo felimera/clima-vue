17de27ee8922f553f9f1d90218e401435751ca78
andres.mera <feli041089@gmail.com>
2023-09-04 13:47:16 -0500

    Consultando el Clima

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -9,8 +9,11 @@ export default function useClima() {
       // Obtener la Lat, Log
       const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
       const { data } = await axios(url);
-      const { la, lon } = data[0];
+      const { lat, lon } = data[0];
       // Obtener el clima
+      const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
+      const { data: resultado } = await axios(urlClima);
+      console.log("resultado", resultado);
     } catch (error) {
       console.log("error", error);
     }
