2b379e22b657e6d4ee899b69b9bb45ba65196c53
andres.mera <feli041089@gmail.com>
2023-09-04 13:57:28 -0500

    Creando el componente para mostrar el clima

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,6 +1,9 @@
+import { computed, ref } from "vue";
 import axios from "axios";
 
 export default function useClima() {
+  const clima = ref({});
+
   const obtenerClima = async ({ ciudad, pais }) => {
     // Importar el Api Key
     const key = import.meta.env.VITE_API_KEY;
@@ -13,12 +16,16 @@ export default function useClima() {
       // Obtener el clima
       const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
       const { data: resultado } = await axios(urlClima);
-      console.log("resultado", resultado);
+      clima.value = resultado;
     } catch (error) {
       console.log("error", error);
     }
+
+    const mostrarClima = computed(() => Object.values(clima.value).length > 0);
   };
   return {
     obtenerClima,
+    clima,
+    mostrarClima,
   };
 }
