205d7236de6117720ed006271bfcdbbd16f824c7
andres.mera <feli041089@gmail.com>
2023-09-04 13:39:27 -0500

    Extrayendo valores de la consulta previa.

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,10 +1,19 @@
+import axios from "axios";
+
 export default function useClima() {
-  const obtenerClima = ({ ciudad, pais }) => {
+  const obtenerClima = async ({ ciudad, pais }) => {
     // Importar el Api Key
     const key = import.meta.env.VITE_API_KEY;
-    // Obtener la Lat, Log
-    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
-    // Obtener el clima
+
+    try {
+      // Obtener la Lat, Log
+      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
+      const { data } = await axios(url);
+      const { la, lon } = data[0];
+      // Obtener el clima
+    } catch (error) {
+      console.log("error", error);
+    }
   };
   return {
     obtenerClima,
